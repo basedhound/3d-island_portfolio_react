3e963f27a023ac8bad8735c9cc2905b88e758db2
Jozef S <franck.vukelic@gmail.com>
2024-02-07 19:11:14 +0700

    Island, Sky, Bird, Plane : OK

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,18 +4,48 @@ import { Suspense, useEffect, useRef, useState } from "react";
 // import sakura from "../assets/sakura.mp3";
 import { HomeInfo, Loader } from "../components";
 // import { soundoff, soundon } from "../assets/icons";
-// import { Bird, Island, Plane, Sky } from "../models";
+import { Bird, Island, Plane, Sky } from "../models";
 
 const Home = () => {
   // const audioRef = useRef(new Audio(sakura));
   // audioRef.current.volume = 0.4;
   // audioRef.current.loop = true;
 
-  // const [currentStage, setCurrentStage] = useState(1);
+  const [currentStage, setCurrentStage] = useState(1);
   const [isRotating, setIsRotating] = useState(false);
   // const [isPlayingMusic, setIsPlayingMusic] = useState(false);
 
+  // Island Position
+  const adjustIslandForScreenSize = () => {
+    let screenScale, screenPosition;
 
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+      screenPosition = [0, -6.5, -43.4];
+    } else {
+      screenScale = [1, 1, 1];
+      screenPosition = [0, -6.5, -43.4];
+    }
+    return [screenScale, screenPosition];
+  };
+
+  // Plane Position
+  const adjustBiplaneForScreenSize = () => {
+    let screenScale, screenPosition;
+
+    // If screen width is less than 768px, adjust the scale and position
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -4, -4];
+    }
+    return [screenScale, screenPosition];
+  };
+
+  const [islandScale, islandPosition] = adjustIslandForScreenSize();
+  const [biplaneScale, biplanePosition] = adjustBiplaneForScreenSize();
 
   return (
     <section className="w-full h-screen relative">
@@ -27,14 +57,39 @@ const Home = () => {
           isRotating ? "cursor-grabbing" : "cursor-grab"
         }`}
         camera={{ near: 0.1, far: 1000 }}>
-        
-        <Suspense fallback={<Loader />}></Suspense>
-        
-
-
-
-
-        </Canvas>
+        <Suspense fallback={<Loader />}>
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <pointLight position={[10, 5, 10]} intensity={2} />
+          <spotLight
+            position={[0, 50, 10]}
+            angle={0.15}
+            penumbra={1}
+            intensity={2}
+          />
+          <hemisphereLight
+            skyColor="#b1e1ff"
+            groundColor="#000000"
+            intensity={1}
+          />
+          <Bird />
+          <Sky isRotating={isRotating} />
+          <Island
+            rotation={[0.1, 4.7077, 0]}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+            setCurrentStage={setCurrentStage}
+            position={islandPosition}
+            scale={islandScale}
+          />
+          <Plane
+            rotation={[0, 20.1, 0]}
+            isRotating={isRotating}
+            scale={biplaneScale}
+            position={biplanePosition}
+          />
+        </Suspense>
+      </Canvas>
     </section>
   );
 };
