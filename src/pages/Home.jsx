909184e6a2474296d4382249abee1ec72f607a1c
Jozef S <franck.vukelic@gmail.com>
2024-02-08 12:02:59 +0700

    Bird OK, InfoBox OK, implementing Contact page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -49,8 +49,9 @@ const Home = () => {
 
   return (
     <section className="w-full h-screen relative">
-      {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">POPUP
-      </div> */}
+      <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        {currentStage && <HomeInfo currentStage={currentStage} />}
+      </div>
 
       <Canvas
         className={`w-full h-screen bg-transparent ${
